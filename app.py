56e188eba6741e603905dd252ba84687362daf55
Andrew Huang <ktpss96034@gmail.com>
2022-09-22 00:33:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
-
 base_url = "https://api2.binance.com"
 api_token = str(os.environ['API_TOKEN'])
 bot = Bot(api_token)
