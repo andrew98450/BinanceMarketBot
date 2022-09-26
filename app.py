1100237953ee557f17e23515883e00012ddeed1d
Andrew Huang <ktpss96034@gmail.com>
2022-09-26 23:17:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ def predictchart(update : Update, context : CallbackContext):
 
     trade_pair = str(context.args[0])
     interval = str(context.args[1])
-    model = MLPRegressor(max_iter=400, activation='relu')
+    model = MLPRegressor(max_iter=200, activation='relu')
     url = base_url + "/api/v3/klines?symbol=%s&interval=%s&limit=1000" % (trade_pair, interval)
     response = requests.get(url=url)
     response_json = response.json()
@@ -268,7 +268,7 @@ def futurechart(update : Update, context : CallbackContext):
 
     trade_pair = str(context.args[0])
     n_day = int(context.args[1])
-    model = MLPRegressor(max_iter=400, activation='relu')
+    model = MLPRegressor(max_iter=200, activation='relu')
     url = base_url + "/api/v3/klines?symbol=%s&interval=1d&limit=1000" % (trade_pair)
     response = requests.get(url=url)
     response_json = response.json()
