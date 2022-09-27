5c3213d07117cf6082935d99051f50bb7cdaadba
Andrew Huang <ktpss96034@gmail.com>
2022-09-27 19:58:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ def futurechart(update : Update, context : CallbackContext):
 
     trade_pair = str(context.args[0])
     n_day = int(context.args[1])
-    model = MLPRegressor(hidden_layer_sizes=(64, 128, 64), max_iter=100, activation='identity')
+    model = MLPRegressor(hidden_layer_sizes=(64, 128, 64), max_iter=500, activation='identity')
     scaler = MinMaxScaler()
     url = base_url + "/api/v3/klines?symbol=%s&interval=1d&limit=1000" % (trade_pair)
     response = requests.get(url=url)
