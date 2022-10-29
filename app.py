a60566fa8e635e1e5793fc2a53a1f9663c06fcf8
Andrew Huang <ktpss96034@gmail.com>
2022-10-29 21:48:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import requests
 import matplotlib.pyplot as plt
 import prettytable as pt
 import pandas
+import tensorflow as tf
 from flask import *
 from telegram import *
 from telegram.ext import *
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 
 base_url = "https://api.binance.com"
@@ -18,8 +18,10 @@ bot = Bot(api_token)
 dispatcher = Dispatcher(bot, None)
 app = Flask(__name__)
 
+
 def start(update : Update, context : CallbackContext):
     help_str = "/start -> Show option menu.\n"
+    help_str += "/generatemodel <trade_pair> -> Generate model.\n"
     help_str += "/priceinfo <trade_pair> ... -> Price information.\n"
     help_str += "/tradeinfo <trade_pair> <n> -> Trade information.\n"
     help_str += "/depthinfo <trade_pair> <n> -> Depth information.\n"
@@ -27,9 +29,39 @@ def start(update : Update, context : CallbackContext):
     help_str += "/tradechart <trade_pair> <n> -> View trade chart.\n"
     help_str += "/depthchart <trade_pair> <n> -> View depth chart.\n"
     help_str += "/klinechart <trade_pair> <interval> -> View kline chart.\n"
-    help_str += "/predictchart <trade_pair> <interval> -> View kline predict chart.\n"
-    help_str += "/futurechart <trade_pair> <n_day> -> View kline future predict chart."
+    help_str += "/predictchart <trade_pair> -> View kline predict chart.\n"
+    help_str += "/futurechart <trade_pair> <n_hour> -> View kline future predict chart."
     update.message.reply_text(help_str)
+
+def generatemodel(update : Update, context: CallbackContext):
+    if len(context.args) < 1:
+        update.message.reply_text("Please input model argument.")
+        return
+
+    username = update.message.from_user.username
+    scaler = MinMaxScaler()
+    trade_pair = str(context.args[0])
+    url = base_url + "/api/v3/klines?symbol=%s&interval=1d&limit=1000" % (trade_pair)
+    response = requests.get(url=url)
+    response_json = response.json()
+    response_json = numpy.array(response_json)[:, 0:5]
+    df = pandas.DataFrame(response_json, columns=['time', 'open', 'high', 'low', 'close'])
+    x_data = numpy.array(df[['open']], dtype=numpy.float32)
+    y_data = numpy.array(df['close'], dtype=numpy.float32)
+
+    x_data = scaler.fit_transform(x_data).reshape((-1, 1, 1))
+    y_data = scaler.fit_transform(y_data.reshape((-1, 1)))
+    model = tf.keras.Sequential([
+        tf.keras.layers.GRU(64, return_sequences=True), 
+        tf.keras.layers.GRU(64),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(1, activation='linear')])
+    model.compile(
+        optimizer="adam", 
+        loss=tf.keras.losses.MeanSquaredError())
+    model.fit(x_data, y_data, batch_size=60, epochs=5, verbose=0, shuffle=True)
+    model.save("%s_%s_model" % (trade_pair, username))
+    update.message.reply_text("ok")
 
 def priceinfo(update : Update, context : CallbackContext):
     if len(context.args) < 1:
@@ -205,63 +237,45 @@ def klinechart(update : Update, context : CallbackContext):
     update.message.reply_photo(open("kline.png", "rb"))
 
 def predictchart(update : Update, context : CallbackContext):
-    if len(context.args) != 2:
+    if len(context.args) != 1:
         update.message.reply_text("Please input predict chart argument.")
         return
 
     trade_pair = str(context.args[0])
-    interval = str(context.args[1])
-    model = MLPRegressor(
-        hidden_layer_sizes=(64, 128, 64), max_iter=500, activation='identity', shuffle=True)
+    username = update.message.from_user.username
     scaler = MinMaxScaler()
-    url = base_url + "/api/v3/klines?symbol=%s&interval=%s&limit=1000" % (trade_pair, interval)
+    url = base_url + "/api/v3/klines?symbol=%s&interval=1d&limit=1000" % (trade_pair)
     response = requests.get(url=url)
     response_json = response.json()
     response_json = numpy.array(response_json)[:, 0:5]
     df = pandas.DataFrame(response_json, columns=['time', 'open', 'high', 'low', 'close'])
 
+    if not os.path.exists("%s_%s_model" % (trade_pair, username)):
+        update.message.reply_text("Please generate pretrained model.")
+        return
+
     time_data = []
     for i, _ in enumerate(df['time'], 0):
         time_data.append(i)
 
+    model = tf.keras.models.load_model("%s_%s_model" % (trade_pair, username))
     x_data = numpy.array(df[['open']], dtype=numpy.float32)
-    y_data = numpy.array(df['close'], dtype=numpy.float32)
     time_data = numpy.array(time_data, dtype=numpy.int32)
-    test_index = int(len(x_data) - (len(x_data) * 0.2))
 
-    x_train, _, y_train, _ = train_test_split(x_data, y_data, test_size=0.2, shuffle=True)
-    time_train, time_test = train_test_split(time_data, test_size=0.2, shuffle=False)
-    x_train = scaler.fit_transform(x_train)
-    y_train = scaler.fit_transform(y_train.reshape((-1, 1))).squeeze()
-    x_test = scaler.fit_transform(x_data[test_index:])
-    model = model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
-
-    data = df[['open', 'high', 'low', 'close']].to_numpy(dtype=numpy.float32)
-    train_data = data[:test_index]
-    test_data = data[test_index:]
-    y_pred = scaler.inverse_transform(y_pred.reshape((-1, 1))).squeeze()
-    kline_open = [num for num in train_data[:, 0]]
-    kline_high = [num for num in train_data[:, 1]]
-    kline_low = [num for num in train_data[:, 2]]
-    kline_close = [num for num in train_data[:, 3]]
-    kline_test_open = [num for num in test_data[:, 0]]
-    kline_test_high = [num for num in test_data[:, 1]]
-    kline_test_low = [num for num in test_data[:, 2]]
-    kline_test_close = [num for num in test_data[:, 3]]
+    _, x_test = train_test_split(x_data, test_size=0.2, shuffle=False)
+    _, time_test = train_test_split(time_data, test_size=0.2, shuffle=False)
+    x_test = scaler.fit_transform(x_test).reshape((-1, 1, 1))
+    y_pred = model.predict(x_test, verbose=0)
+    
+    x_test = scaler.inverse_transform(x_test.reshape((-1, 1)))
+    y_pred = scaler.inverse_transform(y_pred.reshape((-1, 1)))
+    kline_test = [num for num in x_test]
     predict = [num for num in y_pred]
 
     plt.figure()
     plt.title("%s - Predict Chart" % trade_pair)
-    plt.plot(time_train, kline_open, color='g')
-    plt.plot(time_train, kline_high, color='g')
-    plt.plot(time_train, kline_low, color='g')
-    plt.plot(time_train, kline_close, color='g')
-    plt.plot(time_test, kline_test_open, color='r')
-    plt.plot(time_test, kline_test_high, color='r')
-    plt.plot(time_test, kline_test_low, color='r')
-    plt.plot(time_test, kline_test_close, color='r')
-    plt.plot(time_test, predict, color='b')
+    plt.plot(time_test, kline_test, color='g')
+    plt.plot(time_test, predict, color='r')
     plt.tight_layout()
     plt.savefig("predict.png")
     update.message.reply_photo(open("predict.png", "rb"))
@@ -272,9 +286,8 @@ def futurechart(update : Update, context : CallbackContext):
         return
 
     trade_pair = str(context.args[0])
-    n_day = int(context.args[1])
-    model = MLPRegressor(
-        hidden_layer_sizes=(32, 64, 128, 64, 32), max_iter=1000, activation='identity', shuffle=True)
+    n_hour = int(context.args[1])
+    username = update.message.from_user.username
     scaler = MinMaxScaler()
     url = base_url + "/api/v3/klines?symbol=%s&interval=1d&limit=1000" % (trade_pair)
     response = requests.get(url=url)
@@ -282,43 +295,42 @@ def futurechart(update : Update, context : CallbackContext):
     response_json = numpy.array(response_json)[:, 0:5]
     df = pandas.DataFrame(response_json, columns=['time', 'open', 'high', 'low', 'close'])
 
+    if not os.path.exists("%s_%s_model" % (trade_pair, username)):
+        update.message.reply_text("Please generate pretrained model.")
+        return
+
     time_data = []
     for i, _ in enumerate(df['time'], 0):
         time_data.append(i)
 
+    model = tf.keras.models.load_model("%s_%s_model" % (trade_pair, username))
     x_data = numpy.array(df[['open']], dtype=numpy.float32)
-    y_data = numpy.array(df['close'], dtype=numpy.float32)
     time_data = numpy.array(time_data, dtype=numpy.int32)
-    test_index = int(len(x_data) - (len(x_data) * 0.3))
     
-    x_train, _, y_train, _ = train_test_split(x_data, y_data, test_size=0.3, shuffle=True)
-    _, time_test = train_test_split(time_data, test_size=0.3, shuffle=False)
-    x_train = scaler.fit_transform(x_train)
-    y_train = scaler.fit_transform(y_train.reshape((-1, 1))).squeeze()
-    x_test = scaler.fit_transform(x_data[test_index:])
-    model = model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
+    _, x_test = train_test_split(x_data, test_size=0.2, shuffle=False)
+    _, time_test = train_test_split(time_data, test_size=0.2, shuffle=False)
+    x_test = scaler.fit_transform(x_test).reshape((-1, 1, 1))
+    y_pred = model.predict(x_test, verbose=0)
 
     y_future_pred = []
-    x_future = y_pred[-n_day:].reshape((-1, 1))
-    y_hat = model.predict(x_future[0:1])
-    y_hat = y_hat.reshape((-1, 1))
+    x_future = y_pred[-n_hour:].reshape((-1, 1, 1))
+    y_hat = model.predict(x_future[0:1], verbose=0)
+    y_hat = y_hat.reshape((-1, 1, 1))
     tmp_future = y_hat
     y_future_pred.extend(y_hat)
-    for i in range(1, n_day):
+    for i in range(1, n_hour):
         future = numpy.concatenate((x_future[i:i+1], tmp_future), axis=0)
-        y_hat = model.predict(future.reshape((-1, 1)))
-        y_hat = y_hat.reshape((-1, 1))
+        y_hat = model.predict(future.reshape((-1, 1, 1)), verbose=0)
+        y_hat = y_hat.reshape((-1, 1, 1))
         tmp_future = y_hat
         y_future_pred.extend(y_hat)
-    y_future_pred = numpy.array(y_future_pred[-n_day:]).squeeze()
-    time_future = [time_test[-1] + num for num in range(1, n_day + 1)]
+    y_future_pred = numpy.array(y_future_pred[-n_hour:]).squeeze()
+    time_future = [time_test[-1] + num for num in range(1, n_hour + 1)]
 
-    data = df['close'].to_numpy(dtype=numpy.float32)
-    test_data = data[test_index:]
-    y_pred = scaler.inverse_transform(y_pred.reshape((-1, 1))).squeeze()
-    y_future_pred = scaler.inverse_transform(y_future_pred.reshape((-1, 1))).squeeze()
-    kline_test = [num for num in test_data]
+    x_test = scaler.inverse_transform(x_test.reshape((-1, 1)))
+    y_pred = scaler.inverse_transform(y_pred.reshape((-1, 1)))
+    y_future_pred = scaler.inverse_transform(y_future_pred.reshape((-1, 1)))
+    kline_test = [num for num in x_test]
     predict = [num for num in y_pred]
     future_predict = [num for num in y_future_pred]
 
@@ -332,6 +344,7 @@ def futurechart(update : Update, context : CallbackContext):
     update.message.reply_photo(open("future.png", "rb"))
 
 dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("generatemodel", generatemodel))
 dispatcher.add_handler(CommandHandler("priceinfo", priceinfo))
 dispatcher.add_handler(CommandHandler("tradeinfo", tradeinfo))
 dispatcher.add_handler(CommandHandler("depthinfo", depthinfo))
